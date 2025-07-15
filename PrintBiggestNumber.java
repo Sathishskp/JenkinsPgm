eea4f26b0cebf6306dc01f4d4b715a0e07778d44
Sathishskp <164834752+Sathishskp@users.noreply.github.com>
2025-07-15 11:34:54 +0530

    Add files via upload

---
+++ PrintBiggestNumber.java
@@ -0,0 +1,22 @@
+package week2.day1;
+
+public class PrintBiggestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a =100;
+		int b = 400;
+		int c = 300;
+		
+		if(a>b && a>c) {
+			System.out.println(a +" = a is the Biggest Number");
+		}else if(b>a && b>c) {
+			System.out.println(b +" = b is the Biggest Number");
+		}else{
+			System.out.println(c +" =c is the Biggest Number");
+		}
+
+	}
+
+}
